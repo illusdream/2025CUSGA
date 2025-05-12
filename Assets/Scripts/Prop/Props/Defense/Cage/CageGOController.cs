72df17eea697de49f332670f518b9c005524f8b9
illusdream <2312615133@qq.com>
2025-05-12 23:50:40 +0800

    更新特效和物块动画

---
+++ Assets/Scripts/Prop/Props/Defense/Cage/CageGOController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ilsFramework;
+
+namespace Props
+{
+    public class CageGOController : EntityComponent
+    {
+        public const string Usage = "CageGOController";
+
+        public override string TargetUsage => Usage;
+
+        TimerCollection timers = new TimerCollection();
+        
+        public void Start()
+        {
+            if (BuffManager.Instance.TryGetBuffConfig(typeof(CageBuff),out CageBuffConfig buffConfig))
+            {
+                timers.CreateTimer(buffConfig.lastTime,1,"CageGO").SetOnFinish(OnFinish).Register();
+            }
+
+        }
+
+        private void OnFinish(Timer timer)
+        {
+            Destroy(gameObject);
+        }
+        
+        public void OnDestroy()
+        {
+            timers.ClearAllTimers();
+        }
+    }
+}
--- Assets/Scripts/Prop/Props/Defense/Cage/CageProp.cs
+++ Assets/Scripts/Prop/Props/Defense/Cage/CageProp.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using ilsFramework;
+using Unity.Mathematics;
 
 namespace Props
 {
@@ -22,6 +23,11 @@ namespace Props
                         if (CharacterManager.Instance.Player2Controller.handler.TryGetComponet(EntityComponetUsage.Buff,out BaseBuffContainer container))
                         {
                             container.AddBuff(EBuffType.CageBuff);
+                            if (config is CagePropConfig _config)
+                            {
+                                var result = Entity.Instantiate(_config.CageVisual, SpawnSource.SystemGenerate, container.transform.position, quaternion.identity);
+                                result.transform.parent = container.transform;
+                            }
                         }
                     }
                         break;
@@ -30,6 +36,11 @@ namespace Props
                         if (CharacterManager.Instance.Player1Controller.handler.TryGetComponet(EntityComponetUsage.Buff,out BaseBuffContainer container))
                         {
                             container.AddBuff(EBuffType.CageBuff);
+                            if (config is CagePropConfig _config)
+                            {
+                                var result = Entity.Instantiate(_config.CageVisual, SpawnSource.SystemGenerate, container.transform.position, quaternion.identity);
+                                result.transform.parent = container.transform;
+                            }
                         }
                     }
                         break;
